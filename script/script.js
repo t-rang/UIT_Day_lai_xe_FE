5a353c48979e301dd0c8ed4778e9aae46d683074
t-rang <tranhuyentrang1806@gmail.com>
2021-04-23 08:12:19 +0700

    Adjust layout of snippet table and attempt Js code to adjust content of course detail page

--- script/script.js
+++ script/script.js
@@ -1,32 +1,38 @@
-// Courses - Show course preview
+// Index - Courses - Show course preview
 var i = 0;
 var courses = [
     {
+        "id": "0",
         "title": "Bằng B1",
         "img": "image/course-car-b1.1.png",
         "desc": "Bằng lái xe hạng B1 là loại số tự động cấp cho người không hành nghề lái xe để điều khiển ô tô số tự động chở người đến 9 chỗ ngồi, kể cả chỗ ngồi cho người lái xe, ô tô tải, kể cả ô tô tải chuyên dùng số tự động có trọng tải thiết kế dưới 3.500 kg; ô tô dùng cho người khuyết tật.",
     },
     {
+        "id": "1",
         "title": "Bằng B2",
         "img": "image/course-car-b2.png",
         "desc": "Bằng lái B2 cấp cho người hành nghề lái xe để điều khiển ô tô chuyên dùng có trọng tải thiết kế dưới 3.500 kg; các loại xe quy định cho giấy phép lái xe hạng B",
     },
     {
+        "id": "2",
         "title": "Bằng C",
         "img": "image/course-car-c.png",
         "desc": "Bằng lái xe hạng C cấp cho người lái xe để điều khiển ô tô tải, kể cả ô tô tải chuyên dùng, ô tô chuyên dùng có trọng tải thiết kế từ 3.500 kg trở lên, máy kéo kéo một rơ moóc có trọng tải thiết kế từ 3.500 kg trở lên, các loại xe quy định cho giấy phép lái xe hạng B1, bằng lái B2.</li></ul>",
     },
     {
+        "id": "3",
         "title": "Bằng D",
         "img": "image/course-car-d.png",
         "desc": "Bằng lái xe hạng D cấp cho người lái xe để điều khiển các loại xe quy định ở hạng C, lái xe chở người từ 10 – 30 chỗ ngồi. Giấy phép hạng D đòi hỏi người lái xe phải có kinh nghiệm lái và số km an toàn nhất định.",
     },
     {
+        "id": "4",
         "title": "Bằng E",
         "img": "image/course-car-e.png",
         "desc": "Bằng lái xe hạng E cấp cho người lái xe để điều khiển các loại xe quy định ở hạng D, lái xe chở người trên 30 chỗ ngồi. Giấy phép hạng E đòi hỏi người lái xe phải có kinh nghiệm lái và số km an toàn nhất định.",
     },
     {
+        "id": "5",
         "title": "Bằng F",
         "img": "image/course-car-f.png",
         "desc": "Bằng lái xe hạng F cấp cho người lái xe đã có giấy phép lái xe hạng B2, D, E để lái các loại xe quy định cho các giấy phép lái xe hạng này khi kéo rơ moóc hoặc xe ô tô chở khách nối toa. Giấy phép hạng F đòi hỏi người lái xe phải có kinh nghiệm lái và số km an toàn nhất định.",
@@ -37,8 +43,21 @@ function showCoursePreview(i) {
     document.getElementById("course-image").src = courses[i].img;
     document.getElementById("course-desc").innerHTML = courses[i].desc;
 }
+// WIP
+// Course details - Adjust content 
+function adjustCourseDetail() {
+    var title = document.getElementById("course-title").innerHTML;
+    for (i = 0; i < courses.length; i++) {
+        if (title == courses[i].title) {
+            location.replace("course.html");
+            document.getElementById("course-details-img").src = courses[i].img;
+            document.getElementById("course-details-title").innerHTML = courses[i].title;
+            document.getElementById("course-details-desc").innerHTML = courses[i].desc;
+        }
+    }
+}
 
-// Testimony - Show testimony slideshow
+// Index - Testimony - Show testimony slideshow
 var slideIndex = 0;
 showSlides();
 
@@ -59,7 +78,7 @@ function showSlides() {
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
-// Signup - Validate form
+// Index - Signup - Validate form
 function addResult(n) {
     var result = document.createElement("textarea");
     var resultText = document.createTextNode(n);
